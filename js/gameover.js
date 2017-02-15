26aa1dd0c2bbec5baa431859a209b66377fb62a3
krichdev <kgrichardson2@gmail.com>
2017-02-14 19:52:48 -0800

    Display new high score message or display score if not a high score

--- js/gameover.js
+++ js/gameover.js
@@ -15,10 +15,6 @@ gameOver.prototype = {
 
 		game.add.tween(this.overTitle).to({y: 90}, 900).start();
 
-		this.finalScore = game.add.text(325, -250, +score, {font: "30px Arial", fill: "#ffffff"});
-
-		game.add.tween(this.finalScore).to({y: 250}, 900).start();
-
 		this.setHighScore();
 
 	},
@@ -28,8 +24,14 @@ gameOver.prototype = {
 	},	
 
 	setHighScore: function(){
-		if(score > localStorage.highscore)
+		if(score > localStorage.highscore){
 			localStorage.highscore = score;
-	}
+			this.newHighScore = game.add.text(275, -250, "New High Score! " +score, {font: "30px Arial", fill: "#ffffff"});
+			game.add.tween(this.newHighScore).to({y: 350}, 1000).start();
+		} else {
+			this.finalScore = game.add.text(325, -250, +score, {font: "30px Arial", fill: "#ffffff"});
+			game.add.tween(this.finalScore).to({y: 250}, 900).start();
+		}
+	},
 
 }
